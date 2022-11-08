55a46cb64c15765304e9bd564e14db5634a31aed
Havrysha <117733562+Havrysha@users.noreply.github.com>
2022-11-08 17:08:16 +0300

    Update D.py

--- HW1/D/D.py
+++ HW1/D/D.py
@@ -1,3 +1,13 @@
-def solution(total):
-    ### YOUR CODE HERE ###
-    return
+n = int(input('n = '))
+
+def solution(n):
+    hours = n // 60
+    minutes = n % 60
+    if hours <= 24:
+        print(hours, minutes)
+    elif hours > 24:
+        hours = hours - 24
+    
+    
+
+print(solution(n))
