f41ba004a44539119f5b0ee9be52e2e3d618f8ba
Havrysha <117733562+Havrysha@users.noreply.github.com>
2022-11-08 16:48:59 +0300

    Update C.py

--- HW2/C/C.py
+++ HW2/C/C.py
@@ -1,3 +1,9 @@
-def solution(arr):
-    ### YOUR CODE ###
-    return
+n = int(input('n = '))
+k = int(input('k = '))
+
+def solution(n,k):
+    a = k // n
+    b = k % n
+    print(a, b)
+
+print(solution(n,k))
