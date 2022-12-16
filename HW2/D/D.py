e429abab257f7007544dbdf04dda4513a13f6dc6
tim-shlyap <tim-shlyap@yandex-team.ru>
2022-12-17 01:22:44 +0300

    2nd homework

--- HW2/D/D.py
+++ HW2/D/D.py
@@ -1,3 +1,7 @@
 def solution(n,k):
-    ### YOUR CODE HERE ###
-    return
+    i = 1
+    result = 1
+    while i < n:
+        result = (result + k - 1) % (i + 1) + 1
+        i += 1
+    return result
--- HW2/E/E.py
+++ HW2/E/E.py
@@ -1,3 +1,5 @@
 def solution(a, b):
-    ### YOUR CODE ###
-    return
+    if b == 1:
+        return a + 1
+    else:
+        return solution(a, b - 1) + 1
