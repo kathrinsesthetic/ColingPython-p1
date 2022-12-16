912ea8a33a5b6cdba13c05b8b79bb8555bbab541
tim-shlyap <tim-shlyap@yandex-team.ru>
2022-12-17 01:22:44 +0300

    2nd homework

--- HW2/C/C.py
+++ HW2/C/C.py
@@ -1,3 +1,40 @@
 def solution(arr):
-    ### YOUR CODE HERE ###
-    return
+    top, left = -1, -1
+    right = len(arr[0])
+    bottom = len(arr)
+    i, j = 0, 0
+    result = []
+    while right - left > 1 and bottom - top > 1:
+        while j < right:
+            result.append(arr[i][j])
+            j += 1
+        top += 1
+        if bottom - top == 1:
+            break
+        j -= 1
+        i += 1
+        while i < bottom:
+            result.append(arr[i][j])
+            i += 1
+        right -= 1
+        if bottom - top == 1:
+            break
+        i -= 1
+        j -= 1
+        while j > left:
+            result.append(arr[i][j])
+            j -= 1
+        bottom -= 1
+        if bottom - top == 1:
+            break
+        j += 1
+        i -= 1
+        while i > top:
+            result.append(arr[i][j])
+            i -= 1
+        left += 1
+        if bottom - top == 1:
+            break
+        i += 1
+        j += 1
+    return result
